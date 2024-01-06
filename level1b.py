5600b3da2c6dde8f21c23dd4d1c9d356f965d353
unknown <21PT28@cs2k16.amcs.ws2k12.psgcs.edu>
2024-01-06 16:12:54 +0530

    level 1b completed

---
+++ level1b.py
@@ -0,0 +1,255 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import json
+import scipy as sp
+import numpy as np
+from scipy.sparse import csr_matrix
+import copy
+
+from sys import maxsize 
+from itertools import permutations
+
+
+def tsp_greedy(adj, nodes):
+    dense_mat = adj.todense()
+
+    # Converting to dictionary
+    sparse_dict = {}
+    for i in range(dense_mat.shape[0]):
+        indices = np.nonzero(dense_mat[i])[0] # get indices of non-zero elements in row i
+        for j in indices:
+            sparse_dict[(i, j)] = dense_mat[i, j] # use tuple (i, j) as key
+
+
+    keys=(list(sparse_dict.keys()))
+
+    temp1=keys[0]
+
+    visited=[]
+    i=0
+    visited.append(nodes[i])
+    visited=[]
+    unvisited=copy.deepcopy(nodes)
+    visited.append(unvisited[0])
+    unvisited.remove(unvisited[0])
+    
+    #print(visited)
+    #print(unvisited)
+    curr_node=visited[0]
+    for i in range(20):
+        index=nodes.index(curr_node)
+
+        #print(index)
+        lis={}
+        for key in keys:
+            if(key[0]==index):
+                lis[key]=sparse_dict[key]
+        #print(lis)
+
+        while True:
+            minimum=min(lis.values())
+            #print(minimum)
+
+            for key in lis:
+                if(lis[key]==minimum):
+                    min_key=key
+            
+            #print(min_key)
+
+            new_node_index=min_key[1]
+            if(nodes[new_node_index] not in visited):
+                curr_node=nodes[new_node_index]
+                visited.append(curr_node)
+                break
+            else:
+                del(lis[min_key])
+
+    visited.append(nodes[0])
+    #print(visited)  
+    main_dict={}
+
+    sub_dict={}
+
+    sub_dict["path"]=visited
+    #print(sub_dict)  
+
+    main_dict["v0"]=sub_dict
+    #print(main_dict)
+
+    """json_object = json.dumps(main_dict)
+
+    with open("LEVEL_0\level1a.json", "w") as outfile:
+        outfile.write(json_object)"""
+    return main_dict,sparse_dict
+
+
+def find_slots(tot_capacity,path_list,edge_weight_dict,nodes,node_qty):
+
+    start_node=nodes[0]
+  
+    curr_node=start_node
+    visited=[]
+    tot_paths=[]
+    new_ew_dict={}
+    for key in edge_weight_dict.keys():
+        if(key[1]==0):
+            continue
+        else:
+            new_ew_dict[key]=edge_weight_dict[key]
+
+    keys=list(new_ew_dict.keys())
+
+    #print(new_ew_dict)
+    fill=0
+    curr_node=start_node
+    
+    visited.append(curr_node)
+    tot_paths=[]
+    while(len(visited)<51):
+        curr_node=start_node
+        fill=0
+        path=[]
+        path.append("r0")
+        print("\n\ncurr node:",curr_node)
+        while(fill<=tot_capacity):
+            flag=0
+            lis={}
+            index=nodes.index(curr_node)
+            for key in keys:
+                if(key[0]==index):
+                    lis[key]=new_ew_dict[key]
+            #print("list:",lis)
+            while(True):
+                #print("len:" , len(lis.values()))
+                min_val=min(lis.values())
+                #key_lis=list(lis.keys())
+                for key in lis:
+                    if(new_ew_dict[key]==min_val):
+                        min_key=key
+                new_node=min_key[1]
+                if(nodes[new_node] not  in visited):
+                    #print("went inside")
+                    temp_curr_node=nodes[new_node]
+                    fill+=node_qty[temp_curr_node]
+                    if(fill>tot_capacity):
+                        flag=1
+                        break
+                    curr_node=nodes[new_node]
+                    print("fill: ",fill)
+                    visited.append(curr_node)
+                    path.append(curr_node)
+                    break
+                else:
+                    #print("visited:",visited)
+                    #print("Already in visited: ",nodes[min_key[1]])
+                    del(lis[min_key])
+                    continue
+            if(flag==1):
+                continue
+            #print("min_node_out:",nodes[new_node])
+            print("\n\npath:", path)
+            print('len: ',len(visited))
+
+            
+            if(fill>tot_capacity):
+                break
+            if(len(visited)==51):
+                break
+
+        print("visited: ",visited)    
+        
+        tot_paths.append(path)
+    
+    #print(tot_paths)
+
+    for path in tot_paths:
+        path.append("r0")
+
+    print(tot_paths)
+
+    print(len(tot_paths))
+
+    sub_dict={}
+    main_dict={}
+    sub_dict["path1"]=tot_paths[0]
+    sub_dict["path2"]=tot_paths[1]
+    sub_dict["path3"]=tot_paths[2]
+    sub_dict["path4"]=tot_paths[3]
+
+    main_dict["v0"]=sub_dict
+    print(main_dict)
+
+    json_object = json.dumps(main_dict)
+
+    with open("LEVEL_0\level1b_output.json", "w") as outfile:
+        outfile.write(json_object)
+
+   
+
+
+f=open('LEVEL_0\level1b.json')
+
+data=json.load(f)
+
+g=nx.Graph()
+
+nodes=[]
+nodes.append('r0')
+
+for key in data["neighbourhoods"].keys():
+    nodes.append(key)
+
+node=nodes[0]
+
+
+lis=data["restaurants"]["r0"]["neighbourhood_distance"]
+
+j=1
+for i in range(50):
+    g.add_edge(node,nodes[j],weight=lis[i])
+    j+=1
+
+
+for i in range(1,51):
+    lis=data["neighbourhoods"][nodes[i]]["distances"]
+    k=0
+    for j in range(1,len(lis)):
+        if(nodes[i]==nodes[j]):
+            continue
+        g.add_edge(nodes[i],nodes[j],weight=lis[k])
+        k+=1
+
+pos = nx.shell_layout(g)
+
+# Draw the nodes and edges
+nx.draw(g, with_labels=True, node_size=1500, node_color='skyblue', font_size=10)
+edge_labels = nx.get_edge_attributes(g, "weight")
+nx.draw_networkx_edge_labels(g, pos, edge_labels)
+# Show the plot
+plt.show()
+
+
+
+a=nx.adjacency_matrix(g, nodelist=None, dtype=None, weight='weight')
+
+tsp_path_dict,edge_weight_dict=tsp_greedy(a,nodes) 
+
+path_list=tsp_path_dict["v0"]["path"]
+
+tot_qty=data["vehicles"]["v0"]["capacity"]
+print(tot_qty)
+
+print(path_list)
+
+node_qty={}
+
+for i in range(1,len(nodes)):
+    node_qty[nodes[i]]=data["neighbourhoods"][nodes[i]]["order_quantity"]
+
+print(node_qty)
+
+
+find_slots(tot_qty,path_list,edge_weight_dict,nodes,node_qty)
+
+
+
