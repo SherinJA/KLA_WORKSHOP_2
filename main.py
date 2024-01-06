23eb7a4061b25d9a81a8d32eed03e003c25cc6a0
unknown <21PT28@cs2k16.amcs.ws2k12.psgcs.edu>
2024-01-06 11:53:49 +0530

    tsp_greedy

--- main.py
+++ main.py
@@ -2,9 +2,86 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import json
 import scipy as sp
+import numpy as np
+from scipy.sparse import csr_matrix
+import copy
 
 from sys import maxsize 
 from itertools import permutations
+
+
+def tsp_greedy(adj, nodes):
+    dense_mat = adj.todense()
+
+    # Converting to dictionary
+    sparse_dict = {}
+    for i in range(dense_mat.shape[0]):
+        indices = np.nonzero(dense_mat[i])[0] # get indices of non-zero elements in row i
+        for j in indices:
+            sparse_dict[(i, j)] = dense_mat[i, j] # use tuple (i, j) as key
+
+
+    keys=(list(sparse_dict.keys()))
+
+    temp1=keys[0]
+
+    visited=[]
+    i=0
+    visited.append(nodes[i])
+    visited=[]
+    unvisited=copy.deepcopy(nodes)
+    visited.append(unvisited[0])
+    unvisited.remove(unvisited[0])
+    
+    print(visited)
+    print(unvisited)
+    curr_node=visited[0]
+    for i in range(20):
+        index=nodes.index(curr_node)
+
+        print(index)
+        lis={}
+        for key in keys:
+            if(key[0]==index):
+                lis[key]=sparse_dict[key]
+        print(lis)
+
+        while True:
+            minimum=min(lis.values())
+            print(minimum)
+
+            for key in lis:
+                if(lis[key]==minimum):
+                    min_key=key
+            
+            print(min_key)
+
+            new_node_index=min_key[1]
+            if(nodes[new_node_index] not in visited):
+                curr_node=nodes[new_node_index]
+                visited.append(curr_node)
+                break
+            else:
+                del(lis[min_key])
+
+    visited.append(nodes[0])
+    print(visited)  
+    main_dict={}
+
+    sub_dict={}
+
+    sub_dict["path"]=visited
+    print(sub_dict)  
+
+    main_dict["v0"]=sub_dict
+    print(main_dict)
+
+    json_object = json.dumps(main_dict)
+
+    with open("LEVEL_0\my_output_level0.json", "w") as outfile:
+        outfile.write(json_object)
+
+
 
 f=open('LEVEL_0\level0.json')
 
@@ -18,10 +95,8 @@ nodes.append('r0')
 for key in data["neighbourhoods"].keys():
     nodes.append(key)
 
-#print(nodes)
-
 node=nodes[0]
-print(nodes)
+
 
 lis=data["restaurants"]["r0"]["neighbourhood_distance"]
 
@@ -31,12 +106,8 @@ for i in range(20):
     j+=1
 
 
-print(g.edges())
-
 for i in range(1,21):
     lis=data["neighbourhoods"][nodes[i]]["distances"]
-    #print(len(lis))
-    #print(lis)
     k=0
     for j in range(1,len(lis)):
         if(nodes[i]==nodes[j]):
@@ -54,21 +125,14 @@ nx.draw_networkx_edge_labels(g, pos, edge_labels)
 plt.show()
 
 
-print(len(g.edges()))
-
-for node in g.nodes():
-    print(list(g.neighbors(node)))
-
 
 a=nx.adjacency_matrix(g, nodelist=None, dtype=None, weight='weight')
+
+tsp_greedy(a,nodes)
+
 adj=a.todense()
 
-#print(adj)
-#print(type(adj))
-
 adj_list=adj.tolist()
-print(adj_list)
-print(type(adj_list))
 
 
 
